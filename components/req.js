3da745d4a4e3c6c4bff041a5156f63a486d55d01
KHCharan13 <95213528+KHCharan13@users.noreply.github.com>
2023-06-03 01:29:46 +0530

    Update req.js
    ....

--- components/req.js
+++ components/req.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Req({ children, email, uid, name, type }) {
   return (
-    <div className="bg-blue-100/20 w-fit  my-5 md:text-lg text-sm  p-8 shadow-xl rounded-3xl">
+    <div className="bg-blue-100/20 my-5 md:text-lg text-sm  p-8 shadow-xl rounded-3xl">
       <div className="flex mx-[10%] justify-between">
         <div className="flex-col">
           <h1 className="font-bold">Name</h1> <h1 id="name">{name}</h1>
